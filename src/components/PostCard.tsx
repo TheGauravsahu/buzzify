4d2cd347f18b44f3fee0fa502595a982ca938002
Gaurav Sahu <itsgauravsahu7@gmail.com>
2025-03-08 17:59:20 +0530

    feat : add post card, post skeleton

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,159 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+import { Post, User, Comment } from "@prisma/client";
+import { Button } from "./ui/button";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deletePost } from "@/actions/post.action";
+import { toast } from "sonner";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
+import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
+import { formatDate } from "@/lib/utils";
+import { Heart, MessageCircle, SendIcon } from "lucide-react";
+import { Textarea } from "./ui/textarea";
+
+type PostWithRelations = Post & {
+  author: Pick<User, "id" | "name" | "image" | "username">;
+  comments: (Comment & {
+    author: Pick<User, "id" | "name" | "image" | "username">;
+  })[];
+  likes: { userId: string }[];
+  _count: {
+    likes: number;
+    comments: number;
+  };
+};
+
+export default function PostCard({ post }: { post: PostWithRelations }) {
+  const queryClient = useQueryClient();
+  const [showComments, setShowComments] = useState(false);
+  const [newComment, setNewComment] = useState("");
+
+  // delete post mutation
+  const mutation = useMutation({
+    mutationFn: deletePost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return (
+    <Card key={post.id} className="w-full overflow-hidden">
+      <CardHeader>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-4">
+            <Link href={`/profile/${post.author.username}`}>
+              <Avatar className="size-8 sm:w-10 sm:h-10">
+                <AvatarImage src={post.author.image ?? "/avatar.png"} />
+              </Avatar>
+            </Link>
+            <div className="flex flex-col">
+              <Link href={`/profile/${post.author.username}`}>
+                <h2>{post.author.name}</h2>
+              </Link>
+              <h3 className="text-sm text-foreground/60">
+                @{post.author.username}
+              </h3>
+            </div>
+          </div>
+
+          <div className="*:cursor-pointer">
+            <p className="text-xs">{formatDate(post.createdAt)}</p>
+            <Button
+              className="mt-2"
+              variant="secondary"
+              onClick={() => {
+                mutation.mutate(post.id);
+              }}
+            >
+              Delete
+            </Button>
+          </div>
+        </div>
+      </CardHeader>
+
+      <CardContent className="p-4 sm:p-6">
+        <div className="relative h-72 overflow-hidden">
+          <Image
+            fill
+            className="object-cover transition-transform duration-300 group-hover:scale-105"
+            src={post.image as string}
+            alt={post.title}
+          />
+        </div>
+
+        <div className="p-4">
+          <h1>{post.title}</h1>
+          <p className="line-clamp-2">{post.description}</p>
+        </div>
+      </CardContent>
+
+      <CardFooter>
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex items-center w-full gap-4">
+            <Button className="flex items-center gap-2">
+              <Heart size={20} />
+              {post._count.likes}
+            </Button>
+
+            {/* Comment Button */}
+            <Button
+              className="flex items-center gap-2"
+              variant="outline"
+              onClick={() => setShowComments(!showComments)}
+            >
+              <MessageCircle size={20} />
+              {post._count.comments}
+            </Button>
+          </div>
+
+          {showComments && (
+            <div className="mt-4 space-y-4">
+              {post.comments.length > 0 ? (
+                post.comments.map((comment) => (
+                  <div key={comment.id} className="flex items-center gap-4">
+                    <Link href={`/profile/${comment.author.username}`}>
+                      <Avatar className="size-8 sm:w-10 sm:h-10">
+                        <AvatarImage
+                          src={comment.author.image ?? "/avatar.png"}
+                        />
+                      </Avatar>
+                    </Link>
+                    <div>
+                      <Link href={`/profile/${comment.author.username}`}>
+                        <h2>{comment.author.name}</h2>
+                      </Link>
+                      <p>{comment.content}</p>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <p>No comments yet.</p>
+              )}
+            </div>
+          )}
+          <div className="w-full flex gap-4 items-center">
+            <Textarea
+              placeholder="Write a comment..."
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+              className="min-h-[80px] resize-none"
+            />
+
+            <Button size="sm" className="flex items-center gap-2">
+              <>
+                <SendIcon className="size-4" />
+                Comment
+              </>
+            </Button>
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,21 +1,13 @@
 "use client";
 
-import {
-  useInfiniteQuery,
-  useMutation,
-  useQueryClient,
-} from "@tanstack/react-query";
-import { deletePost, getPosts } from "@/actions/post.action";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { getPosts } from "@/actions/post.action";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { Button } from "./ui/button";
-import { toast } from "sonner";
-import Image from "next/image";
-import Link from "next/link";
 import { Skeleton } from "./ui/skeleton";
+import PostCard from "./PostCard";
+import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
 
 export default function PostsList() {
-  const queryClient = useQueryClient();
-
   const { data, fetchNextPage, hasNextPage, status } = useInfiniteQuery({
     queryKey: ["posts"],
     queryFn: ({ pageParam = 0 }) => getPosts({ pageParam }),
@@ -24,18 +16,6 @@ export default function PostsList() {
   });
 
   const posts = data?.pages.flatMap((page) => page.posts) || [];
-  console.log(posts);
-
-  // delete post mutation
-  const mutation = useMutation({
-    mutationFn: deletePost,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["posts"] });
-    },
-    onError: (error) => {
-      toast.error(error.message);
-    },
-  });
 
   if (status === "error") return <div>An error occured.</div>;
 
@@ -49,37 +29,9 @@ export default function PostsList() {
       loader={<PostsListSkeleton />}
       endMessage={<h3>You reached the end.</h3>}
     >
-      <div className="flex flex-wrap gap-8 overflow-hidden min-h-screen">
+      <div className="flex flex-col gap-8 overflow-hidden   w-full md:max-w-4xl mx-auto">
         {posts.map((post) => (
-          <div
-            key={post.id}
-            className="group relative overflow-hidden rounded-lg border bg-card shadow-md transition-all hover:shadow-lg h-96"
-          >
-            <Link href={`/posts/${post.id}`}>
-              <div className="relative h-60 w-full overflow-hidden">
-                <Image
-                  fill
-                  className="object-cover transition-transform duration-300 group-hover:scale-105"
-                  src={post.image as string}
-                  alt={post.title}
-                />
-              </div>
-            </Link>
-            <div className="p-4">
-              <h1>{post.title}</h1>
-              <p className="line-clamp-2">{post.description}</p>
-
-              <Button
-                className="mt-2"
-                variant="secondary"
-                onClick={() => {
-                  mutation.mutate(post.id);
-                }}
-              >
-                Delete
-              </Button>
-            </div>
-          </div>
+          <PostCard post={post} />
         ))}
       </div>
     </InfiniteScroll>
@@ -87,26 +39,51 @@ export default function PostsList() {
 }
 
 const PostsListSkeleton = () => {
-  return (
-    <div className="flex flex-wrap gap-8 mt-8  w-full">
-      {[1, 2, 3, 4].map((i) => (
-        <div
-          key={i}
-          className="group relative overflow-hidden rounded-lg border bg-card shadow-md transition-all hover:shadow-lg h-96"
-        >
-          <div>
-            <Skeleton className="relative h-60 w-[310px] overflow-hidden rounded-none" />
-          </div>
+  const skeletonCount = [1, 2, 3, 4];
 
-          <div className="p-4 ">
+  return (
+    <div className="flex flex-col gap-8 mt-8 justify-center md:justify-normal md:items-start items-center  w-full md:max-w-4xl mx-auto">
+      {skeletonCount.map((i) => (
+        <Card key={i} className="w-full">
+          {/* topbar */}
+          <CardHeader>
+            <div className="flex justify-between items-center p-4">
+              {/* user profile */}
+              <div className="flex gap-4 items-center">
+                <Skeleton className="h-12 w-12 rounded-full" />
+                <div className="flex flex-col gap-2">
+                  <Skeleton className="h-4 w-32" />
+                  <Skeleton className="h-4 w-36" />
+                </div>
+              </div>
+              {/* other info */}
+              <div className="flex flex-col space-y-2">
+                <Skeleton className="h-4 w-20" />
+                <Skeleton className="h-8 w-16" />
+              </div>
+            </div>
+          </CardHeader>
+
+          <CardContent>
+            <div className="h-80 mb-4">
+              <Skeleton className="relative h-full w-full overflow-hidden rounded-none" />
+            </div>
             <Skeleton className="h-4 w-[250px]" />
             <Skeleton className="h-4 w-[200px] my-2" />
+          </CardContent>
 
-            <div className="my-2">
-              <Skeleton className="h-8 w-16" />
+          <CardFooter className="flex flex-col items-start gap-2">
+            <div className="my-2  flex  gap-2">
+              <Skeleton className="h-8 w-12" />
+              <Skeleton className="h-8 w-12" />
             </div>
-          </div>
-        </div>
+
+            <div className="w-full flex items-center gap-4">
+              <Skeleton className="h-28 w-full" />
+              <Skeleton className="h-10 w-24" />
+            </div>
+          </CardFooter>
+        </Card>
       ))}
     </div>
   );
